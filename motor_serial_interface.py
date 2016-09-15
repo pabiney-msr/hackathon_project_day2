098fdd98594060729cf4d23cf5c47daef5754f97
Patrick Abiney <patrickabiney2017@u.northwestern.edu>
2016-09-15 14:46:52 -0500

    things
    made some comments and reformatting and made this not only run one time

--- motor_serial_interface.py
+++ motor_serial_interface.py
@@ -1,7 +1,5 @@
 import serial
 
-
-
 def send_serial(ser, msg):
 	#send serial output and return status bool
 	clear_byte = 0x00		# clear byte, do not change
@@ -10,35 +8,29 @@ def send_serial(ser, msg):
 	data_byte_1 = 0x00		# channel number
 	data_byte_2 = 0x70		# target low bits
 	data_byte_3 = 0x2E		# target high bits
-
+	#package command bytes into string for sending
 	command_string = bytes(chr(clear_byte) + chr(device_byte) + chr(command_byte) + chr(data_byte_1) + chr(data_byte_2) + chr(data_byte_3))
-
-	ser.write(command_string)
-
-	return False
-
+	#write returns the number of bytes written, if that is lessthan or equal to zero we failed so return false
+	return ser.write(command_string) > 0
 
 def read_serial(ser):
 	#read serial input and return value
 	return False
-
 
 def gather_input():
 	#return input
 	msg = raw_input()
 	return msg
 
-
 def handle_input(ser, msg):
 	#ony check first char, bay life
-	if msg.count <= 0:
+	if msg.count <= 0
 		return False
 	if msg[0] == 'X' or  msg[0] == 'x':
 		return False
-	else:
+	else
 		return send_serial(ser, msg[0])
 	return False
-
 
 def loop():
 	#set up serial
@@ -49,10 +41,6 @@ def loop():
 		pass
 	#close serial
 	ser.close()
-
-	return False
-
-
 
 if __name__ == '__main__':
 	loop()
