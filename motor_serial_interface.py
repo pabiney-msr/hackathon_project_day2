6fdcec7bd5f0c6c8ed1ba7520a4391e0fa705897
Nate Kaiser <natejkaiser@gmail.com>
2016-09-15 15:13:38 -0500

    command from serial now working

--- motor_serial_interface.py
+++ motor_serial_interface.py
@@ -1,15 +1,14 @@
 import serial
+import sys
 
 def send_serial(ser, msg):
 	#send serial output and return status bool
-	clear_byte = 0x00		# clear byte, do not change
-	device_byte = 0x00		# device number
-	command_byte = 0x04		# MSB cleared command byte, do not change
-	data_byte_1 = 0x00		# channel number
-	data_byte_2 = 0x70		# target low bits
-	data_byte_3 = 0x2E		# target high bits
+	clear_byte = 0xFF		# clear byte, do not change
+	channel_byte = 0x00		# device's channel number
+	command_byte = 0x7A		# servo position controlled with this
+
 	#package command bytes into string for sending
-	command_string = bytes(chr(clear_byte) + chr(device_byte) + chr(command_byte) + chr(data_byte_1) + chr(data_byte_2) + chr(data_byte_3))
+	command_string = bytes(chr(clear_byte) + chr(channel_byte) + chr(command_byte))
 	#write returns the number of bytes written, if that is lessthan or equal to zero we failed so return false
 	return ser.write(command_string) > 0
 
@@ -24,17 +23,20 @@ def gather_input():
 
 def handle_input(ser, msg):
 	#ony check first char, bay life
-	if msg.count <= 0
+	if msg.count <= 0:
 		return False
 	if msg[0] == 'X' or  msg[0] == 'x':
 		return False
-	else
+	else:
 		return send_serial(ser, msg[0])
 	return False
 
 def loop():
 	#set up serial
 	ser = serial.Serial('/dev/ttyACM0')
+	if not ser.isOpen():
+		print "some horrible warning"
+		sys.exit(42)
 	#input loop
 	send_serial(ser, "nothing here yet, replace later")
 	while handle_input(ser, gather_input()):
